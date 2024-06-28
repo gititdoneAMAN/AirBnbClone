ec43ac19a48d36084ae66f2e8ccfbb2a17f03e06
gititdoneAMAN <amandevbhardwaj@gmail.com>
2024-06-28 23:43:16 +0530

    Added the booking page for the user to view bookings and manipulate it

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Bookings = () => {
-  return <div>Bookings</div>;
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/bookingDetails", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        setBookings(response.data.bookings);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Bookings</h1>
+    </div>
+  );
 };
 
 export default Bookings;
